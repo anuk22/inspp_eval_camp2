b36fd5d9a9c947505a1f54948f621f65ad7a554c
anuk22 <109217414+anuk22@users.noreply.github.com>
2022-08-01 11:21:03 +0530

    question:3

--- indiancurrency.py
+++ indiancurrency.py
@@ -1,6 +1,6 @@
 n=int(input("Enter amount :"))
 dict={2000:0,500:0,200:0,100:0,50:0,20:0,10:0,5:0,2:0,1:0}
-lst=[2000,500,200,100,50,20,10,5,2,1]
+lst=list(dict.keys())
 while(n!=0):
     for i in lst:
         if(n>=i):
